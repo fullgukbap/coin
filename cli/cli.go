f8f354afa92e9e25072f12e6e498ff1a332ca43e
JJerBum <2hanjunbum6@gmail.com>
2023-12-04 19:46:40 +0900

    feat: create cli

---
+++ cli/cli.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/JJerBum/nomadcoin/explorer"
+	"github.com/JJerBum/nomadcoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to NomadCoin\n\n")
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("explorer:   Set the PORT of the server\n")
+	fmt.Printf("rest:       Choose between 'html', and 'rest'\n\n")
+	os.Exit(1)
+}
+
+func Start() {
+
+	if len(os.Args) == 1 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set port of the server")
+	model := flag.String("mode", "rest", "Choose between 'html', and 'rest'")
+
+	flag.Parse()
+
+	switch *model {
+	case "rest":
+		rest.Start(*port)
+	case "port":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+}
