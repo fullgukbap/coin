cce6de7ea0f63949b535a8184f99aa334620802c
JJerBum <2hanjunbum6@gmail.com>
2023-12-04 16:01:35 +0900

    feat: using os.Args for CLI

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"github.com/JJerBum/nomadcoin/rest"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Printf("Welcome to NomadCoin\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
+	fmt.Printf("explorer:   Start the HTML Explorer\n")
+	fmt.Printf("rest:       Start the REST API (recommended)\n\n")
+	os.Exit(1)
+}
+
 func main() {
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
 }
