e1509b6aeaad025faaa9b84e0edc009a10af9577
JJerBum <2hanjunbum6@gmail.com>
2023-12-05 13:43:44 +0900

    feat: create DB()

---
+++ db/db.go
@@ -0,0 +1,35 @@
+// blockchain과 상호작용을 많이 할 패키지이다.
+package db
+
+import (
+	"github.com/JJerBum/nomadcoin/utils"
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbName      = "blockchain.db"
+	dataBucket  = "data"
+	blockBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		// initialize db
+		dbPointer, err := bolt.Open("blockchain.db", 0600, nil)
+		db = dbPointer
+		utils.HandleErr(err)
+		err = db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = tx.CreateBucketIfNotExists([]byte(blockBucket))
+			utils.HandleErr(err)
+			return err
+		})
+		utils.HandleErr(err)
+
+	}
+	return db
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/JJerBum/nomadcoin
 
 go 1.21.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
