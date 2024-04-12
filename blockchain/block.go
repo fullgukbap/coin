cd9036824eaee54a0a953aa280fe19ff4b23e14d
fullgukbap <2hanjunbum6@gmail.com>
2024-04-12 10:18:34 +0900

    feat: toBytes, persist 함수를 추가 및 구현

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,8 +1,12 @@
 package blockchain
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
 	"fmt"
+
+	"github.com/JJerBum/nomadcoin/db"
 )
 
 // Block 구조체는 blockchain의 block을 정의하는 구조체 입니다.
@@ -11,6 +15,17 @@ type Block struct {
 	Hash     string `json:"hash"`
 	PrevHash string `json:"prevHash,omitempty"`
 	Height   int    `json:"height"`
+}
+
+func (b *Block) toBytes() []byte {
+	var blockBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&blockBuffer)
+	encoder.Encode(encoder.Encode(b))
+	return blockBuffer.Bytes()
+}
+
+func (b *Block) persist() {
+	db.SaveBlock(b.Hash, b.toBytes())
 }
 
 // createBlock 함수는 block을 생성해주는 함수 입니다.
@@ -23,5 +38,6 @@ func createBlock(data string, prevHash string, height int) *Block {
 	}
 	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
 	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.persist()
 	return block
 }
