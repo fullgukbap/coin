7148e248c3f23db6f555459f7d13ed575ff806fb
fullgukbap <2hanjunbum6@gmail.com>
2024-04-12 14:10:14 +0900

    feat: Block를 찾는 함수 구현

--- db/db.go
+++ db/db.go
@@ -65,3 +65,13 @@ func Checkpoint() []byte {
 	})
 	return data
 }
+
+func Block(hash string) []byte {
+	var data []byte
+	DB().View(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blockBucket))
+		data = bucket.Get([]byte(hash))
+		return nil
+	})
+	return data
+}
