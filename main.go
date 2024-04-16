b0d3a3bd6623a0a7eb5d01190a6fa58c3a92d8fc
fullgukbap <2hanjunbum6@gmail.com>
2024-04-16 14:20:56 +0900

    feat: 작업증명 로직 간단히 작성 및 구현

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
-	"github.com/fullgukbap/coin/blockchain"
-	"github.com/fullgukbap/coin/cli"
-	"github.com/fullgukbap/coin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	blockchain.Blockchain()
-	defer db.Close()
+	// blockchain.Blockchain()
+	// defer db.Close()
 
-	cli.Start()
+	// cli.Start()
+
+	difficulty := 8
+	target := strings.Repeat("0", difficulty)
+	nonce := 0
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("1"+fmt.Sprint(nonce))))
+
+		if strings.HasPrefix(hash, target) {
+			fmt.Printf("Chache!\ntarget: %s\nnonce: %d\nhash: %s\n\n", target, nonce, hash)
+			return
+		} else {
+			fmt.Printf("mining!\ntarget: %s\nnonce: %d\nhash: %s\n\n", target, nonce, hash)
+			nonce++
+		}
+
+	}
+
 }
