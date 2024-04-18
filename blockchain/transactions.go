58e7b8784da734198a0324f89c63e8c78602b4d4
fullgukbap <2hanjunbum6@gmail.com>
2024-04-18 10:53:21 +0900

    feat: transaction 추가 구현 with mempool

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"errors"
 	"time"
 
 	"github.com/fullgukbap/coin/utils"
@@ -20,6 +21,53 @@ type mempool struct {
 // export인 이유는 초기화 단계를 제어하는 것이 아님(-> 싱글톤 패턴을 사용할 필요가 없음)
 // 메모리에 있는 값을 보여주기만 해줘도 되기 때문이다.
 var Mempool *mempool = &mempool{}
+
+// makeTx 함수는 거래를 생성합니다.
+func makeTx(from /*보내는 이*/, to /*받는 이*/ string, amount /*보낼 돈*/ int) (*Tx, error) {
+	// 돈이 충분한가?
+	if Blockchain().BalanceByAddress(from) < amount {
+		return nil, errors.New("not enough money")
+	}
+
+	var txIns []*TxIn
+	var txOuts []*TxOut
+	total := 0
+
+	oldTxOuts := Blockchain().TxOutsByAddress(from)
+	for _, txOut := range oldTxOuts {
+		if total > amount {
+			break
+		}
+		total += txOut.Amount
+		txIns = append(txIns, &TxIn{txOut.Owner, txOut.Amount})
+	}
+
+	change := total - amount
+	// 값이 0이면 txOuts(from)에 표현하지 않음
+	if change != 0 {
+		txOuts = append(txOuts, &TxOut{from, change})
+	}
+
+	txOuts = append(txOuts, &TxOut{to, amount})
+	tx := &Tx{
+		Id:        "",
+		Timestamp: int(time.Now().Unix()),
+		TxIns:     txIns,
+		TxOuts:    txOuts,
+	}
+	tx.getId()
+	return tx, nil
+}
+
+// addTx 함수는 tx를 하나 만든 뒤, m.Txs에 tx을 추가하는 함수 입니다.
+func (m *mempool) addTx(to string, amount int) error {
+	tx, err := makeTx("nico", to, amount)
+	if err != nil {
+		return err
+	}
+	m.Txs = append(m.Txs, tx)
+	return nil
+}
 
 // Tx 구조체는 거래를 하기 위해 사용되는 정보로 구성됩니다.
 type Tx struct {
